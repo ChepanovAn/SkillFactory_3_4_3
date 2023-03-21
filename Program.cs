6dadc854c884583ccee5cbaa9a9494bb2165689c
Анатолий <Chepanov199287@gmail.com>
2023-03-21 07:20:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_3_4_3
+{
+    class Program
+    {
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
